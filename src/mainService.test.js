0585a1af3a7db23201dbd2e779344b77d4202205
jisuyoun <nigayoun@gmail.com>
2021-02-04 10:41:20 +0900

    fix. initial rendering test

--- src/mainService.test.js
+++ src/mainService.test.js
@@ -1,20 +1,33 @@
-import MainService from './mainService.js';
+import { _ } from 'core-js';
+import MainService from './mainService.ts';
 
-test('adds 1 + 2 to equal 3', () => {
+test('initial rendering test', () => {
+    //given
+	document.body.innerHTML =
+		'<div>' + '  <span id="username" />' + '  <button id="button" />' + '</div>';
+	const target = document.querySelector('div');
+	const service = new MainService(target);
+    const datalist = [1, [2]];
+    
+    //when
+	const subHtml = service.init(datalist);
 
-    document.body.innerHTML =
-    '<div>' +
-    '  <span id="username" />' +
-    '  <button id="button" />' +
-    '</div>';
+    //then
+	expect(subHtml).toContain('<div>12</div>');
+});
 
+test('clickevent', () => {
+    document.body.innerHTML = `
+    <div id="my-number">
+        <div>12341234</div>
+        <div id="click-count">0</div>
+    </div>`;
 
-    const target = document.querySelector("div");
-    const service = new MainService(target)
-    const datalist = [1,[2]];
-    const subHtml = service.init(datalist);
+    const target = document.querySelector('#my-number');
+    const service = new MainService(target);
+    service.on();
 
-    console.log(subHtml)
+    const count = document.querySelector("#click-count").textContent;
+    console.log(count);
 
-    expect(subHtml).toBe('<span>12</span>');
 });
